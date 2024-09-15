526569eb7e60bd5577a3815764f371d53fcb57a9
Youssef-Shehata <31982488+Youssef-Shehata@users.noreply.github.com>
2024-09-15 22:22:48 +0300

    cat-file : added cating a commit and refactored the code

--- src/cat_file.rs
+++ src/cat_file.rs
@@ -9,35 +9,44 @@ use crate::{
 };
 
 pub fn cat_file(option: Option<CatOptions>, sha: &String) -> anyhow::Result<()> {
+    if option.is_none() {
+        bail!("please provide a valid option");
+    };
+    let option = option.unwrap();
     let mut obj = Object::read(sha)?;
     let mut buf = Vec::new();
 
-    let size = obj.size.parse::<usize>().context("failed to read blob size")?;
+    let size = obj
+        .size
+        .parse::<usize>()
+        .context("failed to read blob size")?;
     buf.resize(size, 0);
     let size = obj
         .size
         .parse::<usize>()
         .context("failed to read blob size")?;
     buf.resize(size, 0);
+    obj.buffer.read_exact(&mut buf).context("")?;
+    let content = String::from_utf8_lossy(&buf);
 
     match obj.kind {
-        BlobKind::Blob => {
-            let _ = obj.buffer.read_exact(&mut buf).context("")?;
-            let content = String::from_utf8_lossy(&buf);
+        BlobKind::Blob => match option {
+            CatOptions::PrettyPrint => println!("{}", content.to_string()),
+            CatOptions::ShowType => println!("tree"),
+            CatOptions::ShowSize => println!("{}", size),
+        },
+        BlobKind::Commit => match option {
+            CatOptions::PrettyPrint => println!("{}", content.to_string()),
+            CatOptions::ShowType => println!("commit"),
+            CatOptions::ShowSize => println!("{}", size),
+        },
 
-            match option {
-                Some(op) => match op {
-                    CatOptions::PrettyPrint => println!("{}", content.to_string()),
-                    CatOptions::ShowType => println!("{}", obj.kind),
-                    CatOptions::ShowSize => println!("{}", size),
-                },
-                None => bail!("please provide a valid option"),
-            }
-        }
+        BlobKind::Tree => match option {
+            CatOptions::PrettyPrint => ls_tree(None, &sha)?,
+            CatOptions::ShowType => println!("tree"),
+            CatOptions::ShowSize => println!("{}", size),
+        },
 
-        BlobKind::Tree => ls_tree(None, &sha)?,
-
-        _ => bail!(format!("can't cat a {}", obj.kind)),
     }
 
     Ok(())
